7932f4e88beb7a029968eef1a1bf654488319629
Flying Chalice <flyingchalice@gmail.com>
2022-04-16 15:54:21 +0200

    infrastructure di added

--- src/Identity/Identity.API/Program.cs
+++ src/Identity/Identity.API/Program.cs
@@ -1,5 +1,6 @@
 using Identity.Application.Services;
 using Identity.Application.Services.Implementation;
+using Identity.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddInfrastructure(builder.Configuration["Database:ConnectionString"]);
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddHealthChecks();
 
---
+++ src/Identity/Identity.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿using Identity.Domain.Entities;
+using Identity.Infrastructure.Repositories;
+using Identity.Infrastructure.Repositories.Implementation;
+using Identity.Infrastructure.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Identity.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        private const string MigrationAssemblyName = "Identity.Infrastructure";
+
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+        {
+            services.AddDbContext<IdentityContext>(options =>
+                options.UseSqlServer(connectionString, x => x.MigrationsAssembly(MigrationAssemblyName))
+                    .EnableSensitiveDataLogging()
+                    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking),
+                ServiceLifetime.Transient);
+
+            services.AddTransient<IUnitOfWork, UnitOfWork.UnitOfWork>();
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddIdentity();
+
+            return services;
+        }
+
+        private static IServiceCollection AddIdentity(this IServiceCollection services)
+        {
+            services.AddIdentityCore<User>()
+                .AddEntityFrameworkStores<IdentityContext>();
+
+            return services;
+        }
+    }
+}
