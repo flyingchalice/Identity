97fbac07fa0c66ba59aeb0102fe78f7bc1656406
Flying Chalice <flyingchalice@gmail.com>
2022-04-16 15:09:23 +0200

    add identity context

---
+++ src/Identity/Identity.Infrastructure/IdentityContext.cs
@@ -0,0 +1,28 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.Infrastructure
+{
+    public class IdentityContext : IdentityUserContext<User>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SetIdentityTablesNames(builder);
+        }
+
+        private static void SetIdentityTablesNames(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+            modelBuilder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            modelBuilder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            modelBuilder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+        }
+    }
+}
