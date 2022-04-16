93e9ddd476cd7f3571be2080804d776210a2fd4b
Flying Chalice <flyingchalice@gmail.com>
2022-04-16 15:31:03 +0200

    user service init

---
+++ src/Identity/Identity.Application/Services/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using Identity.Domain.Entities;
+
+namespace Identity.Application.Services
+{
+    public interface IUserService
+    {
+        IEnumerable<User> GetAll();
+    }
+}
---
+++ src/Identity/Identity.Application/Services/Implementation/UserService.cs
@@ -0,0 +1,20 @@
+﻿using Identity.Domain.Entities;
+using Identity.Infrastructure.UnitOfWork;
+
+namespace Identity.Application.Services.Implementation
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UserService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _unitOfWork.UserRepository.GetAll();
+        }
+    }
+}
