6f076ff0da6b309cc1b6fa2a702ffaf624c4f6d5
Flying Chalice <flyingchalice@gmail.com>
2022-04-16 15:38:00 +0200

    inject userservice

--- src/Identity/Identity.API/Program.cs
+++ src/Identity/Identity.API/Program.cs
@@ -1,3 +1,6 @@
+using Identity.Application.Services;
+using Identity.Application.Services.Implementation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,6 +8,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
