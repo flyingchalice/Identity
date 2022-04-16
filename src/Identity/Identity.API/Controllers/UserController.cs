998163d380ebfa8fab0ed7819fdcc1a94467c487
Flying Chalice <flyingchalice@gmail.com>
2022-04-16 15:33:47 +0200

    user controller init

---
+++ src/Identity/Identity.API/Controllers/UserController.cs
@@ -0,0 +1,22 @@
+using Identity.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly IUserService _userService;
+
+    public UserController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public IActionResult Get()
+    {
+        return Ok(_userService.GetAll());
+    }
+}
